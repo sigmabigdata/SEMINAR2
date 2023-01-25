b8a2eab158f5c5222072b69c82a23185fa899bb1
Sigmabigdata <sigmabigdata@gmail.com>
2023-01-25 16:48:43 +0300

    Task 10 complit

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a = (int)Math.Log10(number)-1;
+    Console.WriteLine(a < 0 ? "Второе число отсутствует" 
+        : (number % (int)Math.Pow(10, a + 1) / (int)Math.Pow(10, a)).ToString());
